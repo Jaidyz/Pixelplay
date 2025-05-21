18b94eda0d876692f437144d8dfc4daa7c993024
Jaidyz <jaiderandres12345@gmail.com>
2025-05-20 22:59:09 -0500

    Update Home.jsx

--- src/views/pages/Home.jsx
+++ src/views/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import About from "./../components/about/About"
 import Footer from "./../components/Footer/Footer"
-import Navbar from "./../components/navbar/Navbar"
+import Navbar from "./../components/Navbar/Navbar"
 
 function home() {
   return (
