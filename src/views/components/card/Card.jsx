024714117f24103faa7b261afdc04ee05e0d69df
Jaidyz <jaiderandres12345@gmail.com>
2025-05-20 23:01:19 -0500

    Update Card.jsx

--- src/views/components/card/Card.jsx
+++ src/views/components/card/Card.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Card.css';
+import './card.css';
 import ShoppingCart from '../../../assets/icons/ShoppingCart';
 import HeartIcon from '../../../assets/icons/HeartIcon';
 import { Link } from 'react-router-dom';
