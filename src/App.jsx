5bb454f7288d698f7ad2a04c3f153f6741b6de53
Jaidyz <jaiderandres12345@gmail.com>
2025-06-01 18:41:36 -0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ import ProductoDetalle from "./views/pages/producto-detalle/ProductoDetalle";
 import AdminPanel from "./views/pages/admin-panel/AdminPanel";
 import Success from "./views/pages/success/Success";  
 import Cancel from "./views/pages/cancel/Cancel"; 
-import  ConfigUsuario from "./views/pages/configUsuario/ConfigUsuario";
 
 export default function App() {
   return (
@@ -25,7 +24,6 @@ export default function App() {
         <Route path="/videojuegos/:id/:name" element={<ProductoDetalle />} />
         <Route path="/success" element={<Success />} />
         <Route path="/cancel" element={<Cancel />} />
-        <Route path="/configuracionusuario" element={<ConfigUsuario />} />
       </Routes>
     </HashRouter>
   );
